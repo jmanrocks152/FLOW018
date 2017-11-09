5981aff3c5d65c8945f38ea8c30cd705cde6fffc
jmanrocks152 <jmanrocks152@gmail.com>
2017-11-09 13:46:44 -0600

    Created the Main file for the challenge

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scanner = new Scanner(System.in);
+	    int testCases = scanner.nextInt();
+	    int inputNumber, inputNumberCopy, inputNumberFactorial;
+	    for(int i = 0; i < testCases; i++) {
+	        inputNumber = scanner.nextInt();
+	        inputNumberCopy = inputNumber;
+	        for(int x = 0; x < inputNumber; x++)
+	            inputNumberFactorial = inputNumberCopy * inputNumberCopy--;
+	    }
+	    
+	    System,out.println(inputNumberFactorial);
+	}
+}

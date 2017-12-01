66ccd46592d3fd0ba05a71cc55f8361f3c585255
jmanrocks152 <jmanrocks152@gmail.com>
2017-11-30 18:54:50 -0600

    Updated Main.java so it would compile
    It compiles, but is not a solution

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-
+ 
 class Main
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	public static void main (String[] args)
+	{	
+	    int total = 0;
 	    Scanner scanner = new Scanner(System.in);
 	    int testCases = scanner.nextInt();
-	    int inputNumber, inputNumberCopy, inputNumberFactorial;
+	    int inputNumber, inputNumberCopy;
 	    for(int i = 0; i < testCases; i++) {
 	        inputNumber = scanner.nextInt();
 	        inputNumberCopy = inputNumber;
-	        for(int x = 0; x < inputNumber; x++)
-	            inputNumberFactorial = inputNumberCopy * --inputNumberCopy;
-	    }
-	    
-	    System,out.println(inputNumberFactorial);
+	        for(int x = 0; x < inputNumber; x++) {
+	            total = inputNumberCopy * --inputNumberCopy;
+	    		}
+	    System.out.println(total);
+		}
 	}
-}
+}  
